2f22594a2383758f2b4a5cdb459604f3b7d544bc
fernanev <eva.fernandez-eva@eurecom.fr>
2016-04-22 16:56:21 +0200

    Top-info Updated

--- client/dev/doremus/components/top-info.component.js
+++ client/dev/doremus/components/top-info.component.js
@@ -15,7 +15,7 @@ var TopInfoComponent = (function () {
     TopInfoComponent = __decorate([
         core_1.Component({
             selector: 'top-info',
-            template: "\n  <div class='square-box'>\n    <div class='square-content'>\n    </div>\n  </div>\n  "
+            template: "\n  <div class='square-box'>\n    <div class='square-content'>\n      <div class=\"main-video\">\n        <iframe id=\"video1\" src=\"https://www.youtube.com/embed/PprEQF9C0Qk\" frameborder=\"0\" allowfullscreen></iframe>\n      </div>\n      <div class='main-text'>\n        <h2 id=\"title0\">Improve music description to foster music exchange and reuse</h2>\n        <p>We create a FRBRoo-based data model and multilingual controlled vocabularies specifically designed for music, along with documentation, examples and tutorials.</p>\n        <h2 id=\"title1\">Travel to the heart of the musical archives of major French institutions</h2>\n        <p>We publish musical metadata coming form BnF, Radio France and Philharmonie de Paris on the web of data, and align datasets together with the major hubs of the Linked Open Data cloud.</p>\n        <h2 id=\"title2\">Conect sources, multiply usage, enrich user experience</h2>\n        <p>We develop a musical recommendation system based on the enriched data model and the interconnection of musical catalogs, in order to offer innovative services.</p>\n    </div>\n    </div>\n  </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], TopInfoComponent);
