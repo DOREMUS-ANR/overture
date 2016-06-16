9cf0a6a8f6a83c8e1c517dd0a3fc9178d8c9a1cd
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2016-06-16 13:48:17 +0200

    search button

--- client/src/doremus/components/top-nav/top-nav.component.ts
+++ client/src/doremus/components/top-nav/top-nav.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Router, ROUTER_DIRECTIVES} from '@angular/router';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button/button';
 
 declare var __moduleName: string;
 
@@ -7,7 +8,7 @@ declare var __moduleName: string;
   moduleId: __moduleName,
   selector: 'top-nav',
   templateUrl: 'top-nav.template.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
 
 export class TopNavComponent {
