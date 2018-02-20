5f7e6332f19ddfb379da9b57e11e31071b7cd47f
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2018-02-20 17:53:56 +0100

    fix

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges, Inject, PLATFORM_ID } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
@@ -8,15 +8,6 @@ import { isPlatformBrowser } from '@angular/common';
 const headerOpacityThreshold = 300;
 
 var localStorage;
-if (!isPlatformBrowser(this.platformId))
-  localStorage = {
-    _data: {},
-    setItem: function(id, val) { return this._data[id] = String(val); },
-    getItem: function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
-    removeItem: function(id) { return delete this._data[id]; },
-    clear: function() { return this._data = {}; }
-  };
-else  localStorage = window.localStorage;
 
 @Component({
   moduleId: module.id,
@@ -34,22 +25,15 @@ export class AppComponent {
   constructor(private router: Router,
     private activatedRoute: ActivatedRoute,
     private titleService: Title, private translate: TranslateService,
-    private globals: Globals) {
-    // this language will be used as a fallback when a translation
-    // isn't found in the current language
-    translate.setDefaultLang('en');
-    translate.use('en');
-
-    this.curLang = localStorage.getItem('lang') || translate.getBrowserLang()
-    translate.use(this.curLang);
-    Globals.lang = this.curLang;
-    setTimeout(() => {
-      console.log('Current language:', this.translate.currentLang);
-      localStorage.setItem('lang', this.translate.currentLang)
-    }, 100);
-  }
+    private globals: Globals, @Inject(PLATFORM_ID) private platformId: string) { }
 
   ngOnInit() {
+    this.manageLanguage();
+    this.manageTitle();
+  }
+
+  manageTitle() {
+
     // set title based on the title of the route in app.routes.ts
     this.router.events
       .filter(event => event instanceof NavigationEnd)
@@ -70,6 +54,33 @@ export class AppComponent {
       });
   }
 
+  manageLanguage() {
+    console.log('manage language');
+
+    if (isPlatformBrowser(this.platformId))
+      localStorage = window.localStorage;
+    else localStorage = {
+      _data: {},
+      setItem: function(id, val) { return this._data[id] = String(val); },
+      getItem: function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+      removeItem: function(id) { return delete this._data[id]; },
+      clear: function() { return this._data = {}; }
+    };
+
+    // this language will be used as a fallback when a translation
+    // isn't found in the current language
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+
+    this.curLang = localStorage.getItem('lang') || this.translate.getBrowserLang()
+    this.translate.use(this.curLang);
+    Globals.lang = this.curLang;
+    setTimeout(() => {
+      console.log('Current language:', this.translate.currentLang);
+      localStorage.setItem('lang', this.translate.currentLang)
+    }, 100);
+  }
+
   updateHeaderOpacity(evt) {
     let currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
     let headerOpacity = isNaN(currPos) ? 0 : currPos / headerOpacityThreshold;
@@ -77,6 +88,7 @@ export class AppComponent {
   }
 
   onLangChange() {
+    if (!isPlatformBrowser(this.platformId)) return;
     localStorage.setItem('lang', this.curLang);
     location.reload();
   }
